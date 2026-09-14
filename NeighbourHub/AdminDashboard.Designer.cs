5c6bd4b8542d94c7d675649b449fb3a9a6dd4e89
kazitahmidurrahman01-boop <kazitahmidurrahman01@gmail.com>
2026-09-14 21:46:44 +0600

    24-56060-1

---
+++ NeighbourHub/AdminDashboard.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace NeighbourHub
+{
+    partial class AdminDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnUsers = new System.Windows.Forms.Button();
+            this.btnBuildings = new System.Windows.Forms.Button();
+            this.btnComplaints = new System.Windows.Forms.Button();
+            this.btnPayments = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(205, 118);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(144, 46);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Reports";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(210, 178);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(174, 16);
+            this.lblInfo.TabIndex = 7;
+            this.lblInfo.Text = "Choose a report to generate";
+            // 
+            // btnUsers
+            // 
+            this.btnUsers.Location = new System.Drawing.Point(210, 218);
+            this.btnUsers.Name = "btnUsers";
+            this.btnUsers.Size = new System.Drawing.Size(180, 45);
+            this.btnUsers.TabIndex = 8;
+            this.btnUsers.Text = "User Report";
+            // 
+            // btnBuildings
+            // 
+            this.btnBuildings.Location = new System.Drawing.Point(415, 211);
+            this.btnBuildings.Name = "btnBuildings";
+            this.btnBuildings.Size = new System.Drawing.Size(191, 52);
+            this.btnBuildings.TabIndex = 9;
+            this.btnBuildings.Text = "Building Report";
+            // 
+            // btnComplaints
+            // 
+            this.btnComplaints.Location = new System.Drawing.Point(210, 288);
+            this.btnComplaints.Name = "btnComplaints";
+            this.btnComplaints.Size = new System.Drawing.Size(180, 45);
+            this.btnComplaints.TabIndex = 10;
+            this.btnComplaints.Text = "Complaint Report";
+            // 
+            // btnPayments
+            // 
+            this.btnPayments.Location = new System.Drawing.Point(415, 288);
+            this.btnPayments.Name = "btnPayments";
+            this.btnPayments.Size = new System.Drawing.Size(180, 45);
+            this.btnPayments.TabIndex = 11;
+            this.btnPayments.Text = "Payment Report";
+            // 
+            // AdminDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnUsers);
+            this.Controls.Add(this.btnBuildings);
+            this.Controls.Add(this.btnComplaints);
+            this.Controls.Add(this.btnPayments);
+            this.Name = "AdminDashboard";
+            this.Text = "AdminDashboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnUsers;
+        private System.Windows.Forms.Button btnBuildings;
+        private System.Windows.Forms.Button btnComplaints;
+        private System.Windows.Forms.Button btnPayments;
+    }
+}
---
+++ NeighbourHub/AdminManagement.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace NeighbourHub
+{
+    partial class AdminManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.cmbModule = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(130, 123);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(348, 46);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "Admin Management";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(135, 188);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(180, 16);
+            this.lblInfo.TabIndex = 8;
+            this.lblInfo.Text = "Choose a module to manage";
+            // 
+            // cmbModule
+            // 
+            this.cmbModule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModule.Items.AddRange(new object[] {
+            "Users",
+            "Buildings",
+            "Properties / Flats",
+            "Residents / Tenants",
+            "Managers",
+            "Notices",
+            "Complaints",
+            "Payments / Rent"});
+            this.cmbModule.Location = new System.Drawing.Point(135, 218);
+            this.cmbModule.Name = "cmbModule";
+            this.cmbModule.Size = new System.Drawing.Size(260, 24);
+            this.cmbModule.TabIndex = 9;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(420, 218);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(250, 22);
+            this.txtSearch.TabIndex = 10;
+            this.txtSearch.Text = "Search...";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(135, 288);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 40);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(270, 288);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(120, 40);
+            this.btnEdit.TabIndex = 12;
+            this.btnEdit.Text = "Edit";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(405, 288);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 40);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete";
+            // 
+            // AdminManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.cmbModule);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Name = "AdminManagement";
+            this.Text = "AdminManagement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.ComboBox cmbModule;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
